4360d7ab8df68b48aabed4e98629041cc3cf854a
Ayushmaan Ganguli <ayushmaanganguli64@gmail.com>
2026-08-31 03:22:23 +0530

    Add WordProfiler class to classify word lengths

---
+++ M5.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+class WordProfiler {
+    String review;
+
+    // Constructor
+    WordProfiler(String review) {
+        this.review = review;
+    }
+
+    // Classify word lengths
+    void classifyWordLengths() {
+        String[] words = review.split(" ");
+
+        int shortWords = 0;
+        int mediumWords = 0;
+        int longWords = 0;
+
+        for (String word : words) {
+            int length = word.length();
+
+            if (length <= 4) {
+                shortWords++;
+            } else if (length <= 8) {
+                mediumWords++;
+            } else {
+                longWords++;
+            }
+        }
+
+        System.out.println("Short: " + shortWords);
+        System.out.println("Medium: " + mediumWords);
+        System.out.println("Long: " + longWords);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter movie review: ");
+        String review = sc.nextLine();
+
+        WordProfiler obj = new WordProfiler(review);
+        obj.classifyWordLengths();
+
+        sc.close();
+    }
+}
