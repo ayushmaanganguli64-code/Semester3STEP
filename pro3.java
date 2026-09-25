4e941e98e91acd9a086b502e4d6c0b8c966bb2ab
Ayushmaan Ganguli <ayushmaanganguli64@gmail.com>
2026-09-26 01:26:33 +0530

    Implement abstract GardenTool and its subclasses

---
+++ pro3.java
@@ -0,0 +1,35 @@
+abstract class GardenTool {
+    public abstract String use();[cite: 5]
+}
+
+class CuttingTool extends GardenTool {
+    public CuttingTool() {
+        super();
+    }
+
+    @Override
+    public String use() {
+        return "Using the tool in the garden, blade sharpened first";[cite: 5]
+    }
+}
+
+class Pruner extends CuttingTool {
+    public Pruner() {
+        super();
+    }
+
+    @Override
+    public String use() {
+        return super.use() + ", then trimming branches precisely";[cite: 5]
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        CuttingTool c = new CuttingTool();[cite: 5]
+        System.out.println(c.use());[cite: 5]
+
+        Pruner p = new Pruner();[cite: 5]
+        System.out.println(p.use());[cite: 5]
+    }
+}
